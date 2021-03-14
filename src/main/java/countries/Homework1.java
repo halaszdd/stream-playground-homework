6854bdcc3ab841fb2077c7de89cf6f966160070b
Dániel Halász <halaszdd@gmail.com>
2021-03-14 13:13:30 +0100

    6thpipeline solved

--- src/main/java/countries/Homework1.java
+++ src/main/java/countries/Homework1.java
@@ -55,8 +55,7 @@ public class Homework1 {
      * Returns summary statistics about the number of country name translations associated with each country.
      */
     public IntSummaryStatistics streamPipeline6() {
-        // TODO
-        return null;
+        return countries.stream().mapToInt(c -> c.getTranslations().size()).summaryStatistics();
     }
 
     /**
